41e122200d56df8e945f1b56de450bed460146e7
raulariias <raularias24685@gmail.com>
2023-02-11 18:14:45 -0400

    FIX meta tags

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'QR Code Component',
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html lang="en"><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html lang="en"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
       filename: 'index.html',
       favicon: './src/assets/favicon-32x32.png'
     }),
